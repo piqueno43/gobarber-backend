a42e8b4b72b3ea39b633911622cb26176e7b1850
Edivaldo Silva <piqueno43@gmail.com>
2020-04-24 12:55:15 -0300

    Preparando backend para publicar

---
+++ src/app/models/User.js
@@ -0,0 +1,35 @@
+import Sequelize, { Model } from 'sequelize';
+import bcript from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcript.hash(user.password, 8);
+      }
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassoword(password) {
+    return bcript.compare(password, this.password_hash);
+  }
+}
+export default User;
